49410b28a2e3af3b12df95ef4c5b14ffb6648de0
Yeshihareg-Tadesse <tadehareg@gmail.com>
2024-11-05 15:50:07 +0300

    updated commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import './index.css'
+import Home from './pages/Home/Home'
+
+function App() {
+  return (
+    <>
+      <Home/>
+    </>
+  )
+}
+
+export default App
